34b9269a0e8ab333ab671a41f51631a7ddd5ca20
Joao Barros <joaopilzbarros@gmail.com>
2026-09-18 14:45:45 -0300

    organização

---
+++ aulas/aula009.py
@@ -0,0 +1,9 @@
+#PYTHON
+# TRANSFORMAÇÃO
+# variavel.replace('x', 'y')
+# x.upper()
+# x.lower()
+# x.capitalize()
+# x.strip() -> remove os espaços do começo e final - x.rstrip() [right] - lado diteiro - x.lstrip() [left]
+
+
